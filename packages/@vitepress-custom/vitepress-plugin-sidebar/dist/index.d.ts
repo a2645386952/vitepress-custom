677f77d2c20771c6ba3fc2420af7468d65a03334
huyikai <hyk_tiger@foxmail.com>
2023-04-28 15:23:05 +0800

    Delete packages/@vitepress-custom/vitepress-plugin-sidebar/dist directory

--- packages/@vitepress-custom/vitepress-plugin-sidebar/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { pagesType } from "../types/index";
-/**
- * @param pages
- * @param root
- * @param collapsible
- * @returns {Array}
- */
-export declare const sidebarParser: (pages: Array<pagesType>, root?: string, collapsible?: boolean) => any;
